19c1655d10ea3b10b639c9e1231ab77ee389520f
SofiiaEmelianova <sofia.emelianova15@outlook.com>
2026-08-12 22:59:10 +0930

    Added all 10 htmls files index.html, FAQ.htmlbrowse-listings.html, product-details.html, favourites.html, enquiries.html, messages.html, chat.html, reports-dashboard.html create-listing.html are all done no need for changes unless required for you part

--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-
-# Register your models here.
-from .models import ProductListing
+from .models import ProductListing, Enquiry, Rating, Message, ProductImage
 
 admin.site.register(ProductListing)
+admin.site.register(Enquiry)
+admin.site.register(Rating)
+admin.site.register(Message)
+admin.site.register(ProductImage)
--- listings/urls.py
+++ listings/urls.py
@@ -4,4 +4,13 @@ from . import views
 urlpatterns = [
     path('browse/', views.browse_listings, name='browse_listings'),
     path('create/', views.create_listing, name='create_listing'),
+    path('product/<int:listing_id>/', views.product_details, name='product_details'),
+    path('chat/<int:listing_id>/', views.chat, name='chat'),
+    path('favourite/<int:listing_id>/', views.toggle_favourite, name='toggle_favourite'),
+path('favourites/', views.favourites_list, name='favourites_list'),
+path('messages/', views.messages_list, name='messages_list'),
+path('reports/', views.reports_dashboard, name='reports_dashboard'),
+path('', views.home, name='home'),
+path('faq/', views.faq, name='faq'),
+path('seller-enquiries/', views.seller_enquiries, name='seller_enquiries'),
 ]
